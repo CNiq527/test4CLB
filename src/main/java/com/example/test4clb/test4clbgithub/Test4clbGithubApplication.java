1c736e8b2705e0a6eaa7174fceead9db0926e080
平煜 <taihua.zth@alibaba-inc.com>
2018-08-30 21:10:48 +0800

    init repo v2 :)

---
+++ src/main/java/com/example/test4clb/test4clbgithub/Test4clbGithubApplication.java
@@ -0,0 +1,12 @@
+package com.example.test4clb.test4clbgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test4clbGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Test4clbGithubApplication.class, args);
+	}
+}
